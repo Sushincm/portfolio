6e562f53612af5dcab06b4d0dd57939bb06b0edf
Sushincm <sushinofficial@gmail.com>
2023-09-07 20:41:08 +0530

    php form modified

--- php/form-mail.php
+++ php/form-mail.php
@@ -1,18 +1,29 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Define your email address where you want to receive the form submissions.
+    $to_email = "your_email@example.com";
+
+    // Get form data
+    $name = $_POST["name"];
     $email = $_POST["email"];
-    $subject = $_POST["subject"];
     $message = $_POST["message"];
-    
-    // Set your email address here
-    $to = "sushinofficial@gmail.com";
-    
-    $headers = "From: $email";
-    
-    if (mail($to, $subject, $message, $headers)) {
-        echo "Message sent successfully!";
+
+    // Validate input (you can add more validation)
+    if (empty($name) || empty($email) || empty($message)) {
+        echo "All fields are required.";
     } else {
-        echo "Message failed to send. Please try again later.";
+        // Subject and email headers
+        $subject = "New Contact Form Submission from $name";
+        $headers = "From: $email";
+
+        // Send the email
+        if (mail($to_email, $subject, $message, $headers)) {
+            echo "Thank you! Your message has been sent.";
+        } else {
+            echo "Oops! Something went wrong and we couldn't send your message.";
+        }
     }
 }
+    // If the form was not submitted, you can handle this as needed (e.g., display the form).
+    // Here, we'll simply display an HTML form.
 ?>
